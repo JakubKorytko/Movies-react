a8d34213e7670e5285000612733a3c08ffcf93a8
JTunczyk <jakub.korytko@o2.pl>
2021-10-14 20:15:48 +0200

    links again

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    var moviesComponents = movies.map((x,y) => { return <Movie key={y} image={"/moviesGfx/" + x.image} name={x.name} /> })
+    var moviesComponents = movies.map((x,y) => { return <Movie key={y} image={"./moviesGfx/" + x.image} name={x.name} /> })
     return (
       <div className="App">
         <div id="movies">
